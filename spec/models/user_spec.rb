9bcb4fd6ac2376a2f7d75e45ef495882815edbe4
TakumaShiga <tak07sweetvo@gmail.com>
2021-09-06 13:48:31 +0900

    usersモデルテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,23 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#total_score' do
     let(:user1) { create(:user) }
+    context 'user_scoresテーブルにデータがある場合' do
+      before do
+        create(:user_score, user: user1, score: 4)
+        create(:user_score, user: user1, score: 5)
+        create(:user_score, user: user1, score: 6)
+      end
 
-    before do
-      create(:user_score, user: user1, score: 4)
-      create(:user_score, user: user1, score: 5)
-      create(:user_score, user: user1, score: 6)
+      it 'スコアの合計を取得できる' do
+        expect(user1.total_score).to eq 15
+      end
     end
 
-    it 'スコアの合計を取得できる' do
-      expect(user1.total_score).to eq 15
+    context 'user_scoresテーブルにデータがない場合' do
+      it 'スコアの合計は0である' do
+        expect(user1.total_score).to eq 0
+      end
     end
+
   end
 end
