2d3957466f36de51d711dbee9101af52b599d558
TakumaShiga <tak07sweetvo@gmail.com>
2021-09-15 14:50:39 +0900

    each_ranked_userメソッドを使用

--- app/models/chapter5_part5/ranks_updater.rb
+++ app/models/chapter5_part5/ranks_updater.rb
@@ -6,8 +6,10 @@ module Chapter5Part5
         Rank.delete_all
         Development::UsedMemoryReport.instance.write('after Rank.delete_all')
         # ユーザーごとのスコア合計を降順に並べ替え、そこからランキング情報を再作成する
-      create_ranks
-      
+        create_ranks
+        Development::UsedMemoryReport.instance.write('after create_ranks')
+        update_ranks
+        Development::UsedMemoryReport.instance.write('after update_ranks')
       end
     end
 
@@ -17,6 +19,12 @@ module Chapter5Part5
       User.includes(:user_scores).find_in_batches(batch_size: 30) do |users|
         Rank.import users.select { |user| user.total_score.nonzero? }.map { |user| { user_id: user.id, score: user.total_score } }
       end 
+
+      def update_ranks
+        RankOrderMaker.new.each_ranked_user do |score, rank|
+          Rank.where(dcore: score).update_all(rank: rank)
+        end
+      end
     end
   end
 end
